c58b651e9daef40005c329ab59732a57b1c888f2
Jonas Müller <jonas.mueller@compitencies.com>
2020-06-15 17:21:58 +0200

    Change python path

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 conda activate satisfaction_observer
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo __dir
 python ${__dir}/main.py
 python ${__dir}/main.py --popular=True
 conda deactivate
