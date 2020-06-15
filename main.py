63c4365cf795029166c7d4f6593c1ec67d16db03
Jonas Müller <jonas.mueller@compitencies.com>
2020-06-15 17:47:08 +0200

    Adjust data dirs to absolute path

--- main.py
+++ main.py
@@ -18,17 +18,19 @@ parser.add_argument("--classification_threshold", type=float, default=0.65,
 args = parser.parse_args()
 
 # SETUP PATHS TO DATA DIRECTORIES
-data_dir = "_data"
+root_path = os.path.dirname(__file__)
+data_dir = os.path.join(root_path, "_data")
+display_data_dir = os.path.join(root_path, "display", "data")
 raw_dir = os.path.join(data_dir, "raw")
 cleaned_dir = os.path.join(data_dir, "cleaned")
 analyzed_dir = os.path.join(data_dir, "analyzed")
-target_dir = os.path.join("display", "data", "popular") if args.popular else os.path.join("display", "data", "recent")
+target_dir = os.path.join(display_data_dir, "popular") if args.popular else os.path.join(display_data_dir, "recent")
 
 # SETUP FILE NAMES AND PATHS
 popular_suffix = "_popular" if args.popular else ""
 day_string = (datetime.now() - timedelta(days=args.time_delta)).strftime("%Y-%m-%d")
 
-leaders_file = os.path.join("display", "data", "leaders.json")
+leaders_file = os.path.join(display_data_dir, "leaders.json")
 
 file_name = day_string + popular_suffix + ".json"
 raw_file = os.path.join(raw_dir, file_name)
