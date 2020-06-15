fae5bf6a29063a84ac9be43c3236be7d536f0f32
Jonas Müller <jonas.mueller@compitencies.com>
2020-06-15 17:36:38 +0200

    Adjust run script

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate satisfaction_observer
-python main.py
-python main.py --popular=True
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo $__dir
+python ${__dir}/main.py
+python ${__dir}/main.py --popular=True
 conda deactivate
