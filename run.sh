eea67b0e6b4a74dc88962284a8dee6b2eef57690
Jonas Müller <jonas.mueller@compitencies.com>
2020-06-15 18:16:56 +0200

    Remove echo

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate satisfaction_observer
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo $__dir
 python ${__dir}/main.py
 python ${__dir}/main.py --popular=True
 conda deactivate
